8fade30d736229e657fc4a93d06dc8a69c74cda8
Niels Voss <code@nielsvoss.com>
2024-07-31 00:24:58 -0700

    Bundle Ghostscript and Tesseract in the PyInstaller output

--- package.ps1
+++ package.ps1
@@ -1,3 +1,10 @@
 # ocrmypdf has issues packaging, so we need to follow the flags linked here:
 # https://github.com/ocrmypdf/OCRmyPDF/issues/659#issuecomment-1517712600
-pyinstaller --copy-metadata pikepdf --copy-metadata ocrmypdf --collect-submodules ocrmypdf --collect-datas ocrmypdf.data main.py
+$ghostscriptExecutable = Get-Item $(Get-Command 'gswin64').Source
+$ghostscriptFolder = $ghostscriptExecutable.Directory.Parent.FullName
+$tesseractFolder = "$env:LOCALAPPDATA\Tesseract-OCR"
+pyinstaller `
+    --copy-metadata pikepdf --copy-metadata ocrmypdf --collect-submodules ocrmypdf --collect-datas ocrmypdf.data `
+    --add-data "$($ghostscriptFolder):ghostscript-program" `
+    --add-data "$($tesseractFolder):tesseract-program" `
+    main.py
